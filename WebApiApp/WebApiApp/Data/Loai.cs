04c0345e7622b6ceb397ac258197511e1225da51
hung nguyen <zayphong92@gmail.com>
2023-02-03 10:44:18 +0700

    update  HangHoas default empty array

--- WebApiApp/WebApiApp/Data/Loai.cs
+++ WebApiApp/WebApiApp/Data/Loai.cs
@@ -15,5 +15,12 @@ namespace WebApiApp.Data
 
         // muốn get 1 loại sẽ có N thì sẽ use
         public virtual ICollection<HangHoa> HangHoas { get; set; }
+
+        // trường hợp set HangHoas là default array = [] ko phải là null
+        public Loai()
+        {
+            // có thể dùng HangHoas = new List<HangHoa>(); để set array = []
+            HangHoas = new HashSet<HangHoa>();
+        }
     }
 }
